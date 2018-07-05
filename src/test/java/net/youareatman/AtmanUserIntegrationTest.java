9c560ef0cc8b69edb2a809bc174a88272874e62a
zvetaM <{37927423}+{matevzmarkovic}@users.noreply.github.com>
2018-07-05 20:35:48 +0200

    Additional password hash test.

--- src/test/java/net/youareatman/AtmanUserIntegrationTest.java
+++ src/test/java/net/youareatman/AtmanUserIntegrationTest.java
@@ -1,5 +1,6 @@
 package net.youareatman;
 
+import net.youareatman.helpers.YouAreAtmanHelpers;
 import net.youareatman.model.AtmanUser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,12 +10,20 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AtmanUserIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Test
+    public void hashTest() {
+        assertEquals(YouAreAtmanHelpers.hashPassword("testPassword!!!"),"$2a$10$3zvoe/h3qjUrBG5Gb1dv0uzVUIz2JMhltBVRKtjVaW9DnpA3V941e");
+
+    }
 
     @Test
     public void createAtmanUser() {
