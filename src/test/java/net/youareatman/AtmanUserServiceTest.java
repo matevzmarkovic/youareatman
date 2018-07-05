db786cce04dbab2fedc2ad0a9d5d504baa7417df
zvetaM <{37927423}+{matevzmarkovic}@users.noreply.github.com>
2018-07-05 20:37:21 +0200

    First service tests.

--- src/test/java/net/youareatman/AtmanUserServiceTest.java
+++ src/test/java/net/youareatman/AtmanUserServiceTest.java
@@ -1,19 +1,58 @@
 package net.youareatman;
 
+import net.youareatman.exceptions.GenericYouAreAtmanException;
+import net.youareatman.exceptions.UserManagementException;
+import net.youareatman.model.AtmanUser;
 import net.youareatman.rest.repositories.AtmanUserRepository;
 import net.youareatman.rest.services.AtmanUserService;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 public class AtmanUserServiceTest {
 
-    private AtmanUserService atmanUserService;
+    private AtmanUserService atmanUserServiceMock;
     private AtmanUserRepository atmanUserRepositoryMock;
 
     @Before
     public void setUp() {
         atmanUserRepositoryMock = Mockito.mock(AtmanUserRepository.class);
-        atmanUserService = new AtmanUserService(atmanUserRepositoryMock);
+        atmanUserServiceMock = new AtmanUserService(atmanUserRepositoryMock);
+    }
+
+    @Test
+    public void createAtmanUserSuccessfuly() throws Exception {
+        when(atmanUserRepositoryMock.findById(eq("test@arnes.si"))).thenReturn(Optional.empty());
+        doAnswer(returnsFirstArg()).when(atmanUserRepositoryMock).save(any(AtmanUser.class));
+
+        AtmanUser atmanUser = atmanUserServiceMock.createUser("test@arnes.si", "AAAA");
+        assertEquals("test@arnes.si", atmanUser.getUserEmail());
+        assertNotNull(atmanUser.getPassHash());
+
+    }
+
+    @Test(expected = UserManagementException.class)
+    public void createAtmanUserWithEmptyEmail() throws Exception {
+        atmanUserServiceMock.createUser("","HashPass");
+    }
+
+    @Test(expected = UserManagementException.class)
+    public void createAtmanUserWithExistingEmail() throws Exception {
+        doThrow(new UserManagementException()).when(atmanUserRepositoryMock).findById(eq("test@arnes.si"));
+
+        atmanUserServiceMock.createUser("test@arnes.si","HashPass");
+
     }
 
 }
