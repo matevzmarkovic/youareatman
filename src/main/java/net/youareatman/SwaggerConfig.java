3e32b17fa25939063d0074463f2649995e2f35d5
zvetaM <{37927423}+{matevzmarkovic}@users.noreply.github.com>
2018-07-06 18:19:31 +0200

    Swagger --> providing API info, hiding the ErrorController.

--- src/main/java/net/youareatman/SwaggerConfig.java
+++ src/main/java/net/youareatman/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -15,8 +19,17 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("net.youareatman.rest.controllers"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "You Are Atman REST API",
+                "Backend API for helping people track their daily spiritual work and ego problems.",
+                "V1",
+                "",
+                new Contact("Matevž Markovič", "https://www.linkedin.com/in/matevz-markovic/", "matevz.markovic@arnes.si"),"","",Collections.emptyList());
     }
 }
