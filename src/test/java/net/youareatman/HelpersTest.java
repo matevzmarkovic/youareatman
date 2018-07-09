cee025db112b5102aac7609585897f2aac91a284
zvetaM <{37927423}+{matevzmarkovic}@users.noreply.github.com>
2018-07-09 09:03:19 +0200

    Fixing HelpersTest.

--- src/test/java/net/youareatman/HelpersTest.java
+++ src/test/java/net/youareatman/HelpersTest.java
@@ -8,14 +8,30 @@ import net.youareatman.model.AtmanUser;
 import net.youareatman.model.IncidentEntry;
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class HelpersTest {
+
+    private static String EMAIL_INVALID_EMPTY = "";
+    private static String EMAIL_INVALID_REGEX = "marka";
+    private static String EMAIL_CORRECT = "marka@arnes.si";
+
+    private static String PASSWORD_HASH_EMPTY = "";
+    private static String PASSWORD_HASH_TOO_SHORT = "passwordWaaayToooShort";
+    private static String PASSWORD_HASH_CORRECT = "3$hS&\\D#rcv9VHtacN7*<Mj{qPLA@?S6zdB^^4RZ\"XM)u@BThVmj#;t+7fYj";
+
+    private static String PASSWORD_EMPTY = "";
+
+    private static String INCIDENT_ID_EMPTY = "";
+    private static String INCIDENT_ID_CORRECT = "id0";
 
     @Test
     public void testValidateUserEmail_isEmpty() {
         try {
-            YouAreAtmanHelpers.validateUserEmail("");
+            YouAreAtmanHelpers.validateUserEmail(EMAIL_INVALID_EMPTY);
         }
         catch(UserManagementException e) {
             assertEquals(e.getErrorType(),ErrorTypesEnum.EmptyUserEmailError);
@@ -27,47 +43,55 @@ public class HelpersTest {
             assertEquals(e.getErrorType(),ErrorTypesEnum.EmptyUserEmailError);
         }
     }
-
     @Test
     public void testValidateUserEmail_invalidRegex() {
         try {
-            YouAreAtmanHelpers.validateUserEmail("abc");
+            YouAreAtmanHelpers.validateUserEmail(EMAIL_INVALID_REGEX);
         }
         catch(UserManagementException e) {
             assertEquals(e.getErrorType(),ErrorTypesEnum.InvalidUserEmailError);
         }
         try {
-            YouAreAtmanHelpers.validateUserEmail_Incident("abc");
+            YouAreAtmanHelpers.validateUserEmail_Incident(EMAIL_INVALID_REGEX);
         }
         catch(IncidentManagementException e) {
             assertEquals(e.getErrorType(),ErrorTypesEnum.InvalidUserEmailError);
         }
     }
+    //No exception should be thrown. If it is, something is wrong with email validation.
+    @Test(expected = Test.None.class)
+    public void testValidateUserEmail_correctEmail() throws UserManagementException {
+        YouAreAtmanHelpers.validateUserEmail(EMAIL_CORRECT);
+    }
 
     @Test
     public void testValidatePasswordHash_isEmpty() {
         try {
-            YouAreAtmanHelpers.validatePasswordHash("");
+            YouAreAtmanHelpers.validatePasswordHash(PASSWORD_HASH_EMPTY);
         }
         catch(UserManagementException e) {
             assertEquals(e.getErrorType(),ErrorTypesEnum.EmptyPasswordHashError);
         }
     }
-
     @Test
     public void testValidatePasswordHash_isInvalid() {
         try {
-            YouAreAtmanHelpers.validatePasswordHash("passwordWaaayToooShort");
+            YouAreAtmanHelpers.validatePasswordHash(PASSWORD_HASH_TOO_SHORT);
         }
         catch(UserManagementException e) {
             assertEquals(e.getErrorType(),ErrorTypesEnum.MalformedPasswordHashError);
         }
     }
+    //No exception should be thrown. If it is, something is wrong with password hash validation.
+    @Test(expected = Test.None.class)
+    public void testValidatePasswordHash_correctPassword() throws UserManagementException {
+        YouAreAtmanHelpers.validatePasswordHash(PASSWORD_HASH_CORRECT);
+    }
 
     @Test
     public void testValidatePassword_isEmpty() {
         try {
-            YouAreAtmanHelpers.validatePasswordRaw("");
+            YouAreAtmanHelpers.validatePasswordRaw(PASSWORD_EMPTY);
         }
         catch(UserManagementException e) {
             assertEquals(e.getErrorType(),ErrorTypesEnum.EmptyPasswordError);
@@ -94,23 +118,24 @@ public class HelpersTest {
     @Test
     public void testValidateIncidentId_isEmpty() {
         try {
-            YouAreAtmanHelpers.validateIncidentId("");
+            YouAreAtmanHelpers.validateIncidentId(INCIDENT_ID_EMPTY);
         }
         catch(IncidentManagementException e) {
             assertEquals(e.getErrorType(),ErrorTypesEnum.EmptyIncidentIdError);
         }
     }
 
-    @Test
-    public void testValidateIncidentEntry() {
-        IncidentEntry entry = new IncidentEntry("id0",null, new AtmanUser("whateverUserNotEmail","passHash"),"","","");
-
-        try {
-            YouAreAtmanHelpers.validateIncidentEntry(entry);
-        }
-        catch(IncidentManagementException e) {
-            return;
-        }
+    @Test(expected = IncidentManagementException.class)
+    public void testValidateIncidentEntry_invalid() throws IncidentManagementException {
+        IncidentEntry entry = new IncidentEntry(INCIDENT_ID_EMPTY,null, new AtmanUser(EMAIL_INVALID_EMPTY, PASSWORD_HASH_TOO_SHORT),"","","");
+        YouAreAtmanHelpers.validateIncidentEntry(entry);
+    }
+    //No exception should be thrown. If it is, something is wrong with incident entry validation.
+    @Test(expected = Test.None.class)
+    public void testValidateIncidentEntry_correct() throws IncidentManagementException {
+        Date incidentDate = new Date();
+        IncidentEntry entry = new IncidentEntry(INCIDENT_ID_CORRECT,incidentDate, new AtmanUser(EMAIL_CORRECT, PASSWORD_HASH_CORRECT),"","","");
+        YouAreAtmanHelpers.validateIncidentEntry(entry);
     }
 
 }
